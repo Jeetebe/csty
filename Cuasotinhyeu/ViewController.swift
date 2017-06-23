3f5ddc653d98850bdc49157b520c83e47d12c86a
Jeetebe <jeetebe@Jeetebes-Mac.local>
2017-06-23 11:40:19 +1000

    Initial Commit

---
+++ Cuasotinhyeu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cuasotinhyeu
+//
+//  Created by Jeetebe on 23/6/2017.
+//  Copyright © 2017 Jeetebe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
